348c9d50058f933ba9c04712b9ead9372245e457
yashsj <yashsj@yahoo.co.in>
2025-01-15 17:02:17 -0600

    Update 112_LC_Path_sum.py

--- 112_LC_Path_sum.py
+++ 112_LC_Path_sum.py
@@ -15,3 +15,21 @@ class Solution:
         left_sum=self.hasPathSum(root.left, targetSum-root.val)
         right_sum= self.hasPathSum(root.right, targetSum-root.val)
         return left_sum or right_sum
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool: 
+        def dfs(node,total_sum):
+            if not node:
+                return False
+            total_sum+=node.val
+            if not node.left and not node.right:
+                return total_sum == targetSum
+            return (dfs(node.left,total_sum) or dfs(node.right,total_sum))
+        return dfs(root,0)

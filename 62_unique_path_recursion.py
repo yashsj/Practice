f9eee3564874ff912f5b7a0ea539d73c160875ba
yashsj <yashsj@yahoo.co.in>
2025-02-02 23:52:11 -0600

    Update 62_unique_path_recursion.py

--- 62_unique_path_recursion.py
+++ 62_unique_path_recursion.py
@@ -14,3 +14,14 @@ class Solution:
         return dfs(0,0)
             #cache[r][c]=
         
+#DP Bottom-up apprach
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]

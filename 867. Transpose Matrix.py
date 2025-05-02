5b100ac3e7296ba4f28919806161b2b8911dcd21
yashsj <yashsj@yahoo.co.in>
2025-05-01 22:34:07 -0500

    Create 867. Transpose Matrix.py

---
+++ 867. Transpose Matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        ROWS,COLS=len(matrix),len(matrix[0])
+        res=[[0]*(ROWS) for _ in range(COLS)]
+        for r in range(ROWS):
+            for c in range(COLS):
+                res[c][r]=matrix[r][c]
+        return res
+        

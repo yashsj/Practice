c3e3b4216418adb7c11d20c4d5ff4bf5eb3bee4c
yashsj <yashsj@yahoo.co.in>
2025-08-17 23:35:00 -0500

    Create 2176.py

---
+++ 2176.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count=0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j] and (i*j)%k==0:
+                    count=count+1
+        return count
+
+
+
+
+        

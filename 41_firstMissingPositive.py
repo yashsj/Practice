24681f2b6bc03584f221387e2d677ba16c2c6102
yashsj <yashsj@yahoo.co.in>
2025-08-29 23:58:55 -0500

    Update 41_firstMissingPositive.py

--- 41_firstMissingPositive.py
+++ 41_firstMissingPositive.py
@@ -2,7 +2,7 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         for i in range(len(nums)):
             if nums[i]<0:
-                nums[i]=nums[i]*-1
+                nums[i]=0
         
         for i in range(len(nums)):
             val=abs(nums[i])
